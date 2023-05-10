b6022a7abbe2ddee9346a1aabac9ac0b985f224a
Lida Susan Sabu <lidasusansabu13.1@gmail.com>
2023-05-10 06:20:14 -0400

    moved files to a folder named github web service, changed gitignore, fixed import error in app.vue

---
+++ github-web-service/src/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+//import RouterLink  from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import FirstPage from './pages/ProjectsPage.vue'
+import PullRequestsPage from './pages/PullRequestsPage.vue'
+
+</script>
+
+<template>
+  
+  <header>
+    <h1>GitManager</h1>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/pr">Pull Requests</RouterLink>
+        <RouterLink to="/issues">Issues</RouterLink>
+      </nav>
+      <hr/>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+
+</style>
